f602a0aba0690a3eefe32a71d159b0b5827f3a44
Vivian Wong <vivianwong@vivians-macbook.local>
2018-11-15 10:56:48 -0800

    Initial Commit

---
+++ KumQuat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KumQuat
+//
+//  Created by Vivian Wong on 11/15/18.
+//  Copyright © 2018 Vivian Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
